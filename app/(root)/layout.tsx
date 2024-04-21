94d1d69d7019af8cef41fbfec8255bc17a623c38
Lailatul Fitriyah <arwahitem01@gmail.com>
2024-04-21 15:30:22 +0700

    add onboard page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
-import Topbar from "@/component/shared/Topbar";
-import LeftSidebar from "@/component/shared/LeftSidebar";
-import Bottombar from "@/component/shared/Bottombar";
-import RightSidebar from "@/component/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -38,7 +39,6 @@ export default function RootLayout({
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
-            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
---
+++ schema/types.ts
@@ -0,0 +1,11 @@
+export interface AccountProps {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+      };
+      btnTitle: string;
+}
